19f3e2d6b758d5f8b023c8ffef5d0c7f9fa0f35e
ramy1415 <ramy1415@hotmail.com>
2020-06-05 15:15:36 +0200

    checkrole middleware and making admin register not logging in after registeration

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,11 +6,32 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Auth\RegisterController;
-
+use Illuminate\Http\Response;
+use Illuminate\Auth\Events\Registered;
 class AdminController extends RegisterController
 {
-    public function showRegistrationForm()
-        {
+
+    public function __construct(){
+        $this->middleware('check-role:admin');
+    }
+
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        if ($response = $this->registered($request, $user)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+                    ? new Response('', 201)
+                    : redirect($this->redirectPath());
+    }
+
+    
+    public function showRegistrationForm(){
             return view('auth.allregister',[
                 'route'=>'admin.register',
                 'role'=>'Admin'
