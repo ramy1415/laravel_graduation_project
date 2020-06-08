42ec0c0efa0d47de9fb68f27ec0b7eece2727a9f
MahmoudEzz <mahmoud.ezz49@gmail.com>
2020-06-08 17:24:17 +0200

    Home page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Design;
 use App\Tag;
+use App\User;
 class IndexController extends Controller
 {
     public function index(){
@@ -12,7 +13,11 @@ class IndexController extends Controller
         $latestDesigns = Design::orderBy('id', 'desc')->take(8)->get();
         $tags = Tag::get()->all();
         $topDesigns = Design::orderBy('total_likes', 'desc')->take(8)->get();
-        return view('welcome', compact('latestDesigns', 'tags', 'topDesigns'));
+        $companies = User::where('role', '=', 'company')->get();
+
+        $role = is_null(auth()->user()) ? 'm3l4' : auth()->user()->role;
+         
+        return view('welcome', compact('latestDesigns', 'tags', 'topDesigns', 'companies', 'role'));
     }
 
     
