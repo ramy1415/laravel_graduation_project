572b4c523fc2cb3caaa59cf26650b42d75166479
ramy1415 <ramy1415@hotmail.com>
2020-06-13 01:36:53 +0200

    company add design form

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\CompanyDesign;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Response;
+
 
 class CompanyController extends Controller
 {
+    public function __construct() {
+        $this->middleware(['auth','check-role:company'], ['only' => ['show_create_design_form']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -19,24 +28,53 @@ class CompanyController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new desing.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function show_create_design_form()
     {
         //
+        $company = Auth::user();
+        return view('companies.add_design',compact(['company']));
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created design in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function create_design(Request $request)
     {
         //
+        // dd($request->all());
+        $this->design_validator($request->all())->validate();
+        event(new Registered($design = $this->create_new_design($request->all())));
+        return $request->wantsJson()
+                    ? new Response('', 201)
+                    : redirect('/');
+
+    }
+
+    protected function design_validator(array $data)
+    {
+        return Validator::make($data, [
+            'design' => ['required'],
+            'link' => ['required'],
+            'image'=>['required']
+        ]);
+    }
+
+    protected function create_new_design($data)
+    {
+        $image_path = $data['image']->store('uploads', 'public');
+
+        return CompanyDesign::updateOrCreate(['design_id' => $data['design']],[
+            'company_id'=>Auth::user()->id,
+            'link' => $data['link'],
+            'image' => $image_path,
+        ]);
     }
 
     /**
