01f2fda3b4fff33665182e4fefe65cd16f5fc8db
ramy1415 <ramy1415@hotmail.com>
2020-06-18 09:28:04 +0200

    removing duplicate notification table

--- public/js/app.js
+++ public/js/app.js
@@ -43320,8 +43320,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "f6281f4e9f8d2a800dd2",
-  cluster: "eu",
+  key: "",
+  cluster: "mt1",
   forceTLS: true
 });
 
