a399a2ab5898477ba7547497515cc45f0c02f505
Ghada Mustafa <ghadamostafa28@gmail.com>
2020-06-24 18:33:46 +0200

    designs images carousel

--- resources/views/dashboard/verify_designs.blade.php
+++ resources/views/dashboard/verify_designs.blade.php
@@ -4,13 +4,13 @@
 
 @csrf
 <!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800">Verify {{$state}} Designs</h1>
+<h1 class="h3 mb-2 text-gray-800">Verify {{$state}} designs</h1>
 <p class="mb-4">This is a list of designs which needs to be verified.</a>.</p>
 
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
   <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary">Designs Table</h6>
+    <h6 class="m-0 font-weight-bold text-primary">Pending designs Table</h6>
   </div>
   <div class="card-body">
     <div class="table-responsive">
@@ -39,13 +39,15 @@
                 <td class="align-middle">{{$design->price}}</td>
                 <td class="align-middle">{{$design->category}}</td>
                 <td class="align-middle">
-                    @forelse ($design->images as $image)
-                        <img class="img-thumbnail" style="width: 100px; height:100px" src={{asset('storage/'.$image->image)}} alt="" srcset="">
-                    @empty
-                        <div class="alert alert-danger" role="alert">
-                            <strong>No Images yet</strong>
-                        </div>
-                    @endforelse
+                    <a href="#"  data-toggle="modal" data-target="#ImagesModal{{ $design->id }}" >
+                        @forelse ($design->images as $image)
+                            <img class="img-thumbnail" style="width: 100px; height:100px" src={{asset('storage/'.$image->image)}} alt="" srcset="">
+                        @empty
+                            <div class="alert alert-danger" role="alert">
+                                <strong>No Images yet</strong>
+                            </div>
+                        @endforelse
+                    </a>
                 </td>
                 <td class="align-middle">
                     <a target="_blank" href="{{route('admin.view_design_document',$design->id)}}" class="btn btn-success ">Preview Document</a>
@@ -57,10 +59,73 @@
                 @endif
                 @if($state === 'accepted' || $state === 'pending')
                     <td class="align-middle">
-                        <button type="button" class="btn btn-danger"  data-toggle="modal" data-target="#RejectionModal" id="{{ $design->id }}">Reject</button>
+                        <button type="button" class="btn btn-danger"  data-toggle="modal" data-target="#RejectionModal{{ $design->id }}" id="{{ $design->id }}">Reject</button>
                     </td>
                 @endif
             </tr>
+            <!-- Rejection Modal -->
+            <div class="modal fade" id="RejectionModal{{ $design->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Design Confirmation </h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                               
+                        <div class="modal-body">
+                            <div class="alert alert-danger" style="display:none"></div>
+                            <form class="form-horizontal" role="form"  method="post" action="#">
+                                @csrf
+                                            
+                                <input type="text" placeholder="To" name="To" value="{{ $design->designer->email }}" class="form-control  reciever" autofocus>
+                                <input type="text" placeholder="Subject" name="Subject"  class="form-control mt-2 Subject" autofocus>
+                                <input type="hidden" value="{{$design->id}}" id="design_id">
+                                <textarea  name="Message" placeholder="Message" class="form-control mb-2 mt-2 Message" rows="4" cols="50" autofocus></textarea>
+                                              
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary" type="submit" onclick="change_verification($('#'+{{ $design->id }}),{{$design->id}},'rejected')" >Send</button>
+                                </div>
+                             </form>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+             <!-- Images modal -->
+            <div class="modal fade" id="ImagesModal{{ $design->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+              <div class="modal-dialog  modal-lg" >
+                <div class="modal-content">
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                      <!-- Wrapper for slides -->
+
+                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                  <img class="d-block w-100" src="{{asset ('storage/'.$design->images->first()->image) }}" alt="First slide">
+                                </div>
+                                @foreach ($design->images as  $key => $image)
+                                    @if($key>0)
+                                        <div class="carousel-item ">
+                                          <img class="d-block w-100" src="{{asset ('storage/'.$image->image) }}" alt="First slide">
+                                        </div>
+                                    @endif
+                                @endforeach
+                            </div>
+                          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" style="color: black;">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" style="color: black;">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                          </a>
+                        </div>
+
+                </div>
+              </div>
+            </div>
             @empty
             <tr>
                 <td colspan="8">
@@ -72,36 +137,7 @@
             @endforelse
         </tbody>
 </table>
-<!-- Modal -->
-    <div class="modal fade" id="RejectionModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Design Confirmation </h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                       
-                <div class="modal-body">
-                    <div class="alert alert-danger" style="display:none"></div>
-                    <form class="form-horizontal" role="form"  method="post" action="#">
-                        @csrf
-                                    
-                        <input type="text" placeholder="To" name="To" value="{{ $design->designer->email }}" class="form-control  reciever" autofocus>
-                        <input type="text" placeholder="Subject" name="Subject"  class="form-control mt-2 Subject" autofocus>
-                        <input type="hidden" value="{{$design->id}}" id="design_id">
-                        <textarea  name="Message" placeholder="Message" class="form-control mb-2 mt-2 Message" rows="4" cols="50" autofocus></textarea>
-                                      
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" type="submit" onclick="change_verification($('#'+{{ $design->id }}),{{$design->id}},'rejected')" >Send</button>
-                        </div>
-                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
+
 {{ $designs->links() }}
 
 </div>
@@ -151,7 +187,7 @@
                         else
                         {
                             $('.alert-danger').hide();
-                            $('#RejectionModal').modal('hide');
+                            $('#RejectionModal'+design_id).modal('hide');
                             $(btn).parents('tr').hide('1000'); 
                         }
                         console.log(data);
