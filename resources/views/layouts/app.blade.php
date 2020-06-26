c2789f4c426866ff5c2fda95f14e3321174f2b26
Ghada Mustafa <ghadamostafa28@gmail.com>
2020-06-26 12:45:52 +0200

    edit comment replies

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -240,16 +240,6 @@
     <section class="footer-section">
         <div class="social-links-warp">
             <div class="container">
-                <div class="social-links">
-                    <a href="" class="instagram"><i class="fa fa-instagram"></i><span>instagram</span></a>
-                    <a href="" class="google-plus"><i class="fa fa-google-plus"></i><span>g+plus</span></a>
-                    <a href="" class="pinterest"><i class="fa fa-pinterest"></i><span>pinterest</span></a>
-                    <a href="" class="facebook"><i class="fa fa-facebook"></i><span>facebook</span></a>
-                    <a href="" class="twitter"><i class="fa fa-twitter"></i><span>twitter</span></a>
-                    <a href="" class="youtube"><i class="fa fa-youtube"></i><span>youtube</span></a>
-                    <a href="" class="tumblr"><i class="fa fa-tumblr-square"></i><span>tumblr</span></a>
-                </div>
-
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> 
 <p class="text-white text-center mt-5">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</p>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
